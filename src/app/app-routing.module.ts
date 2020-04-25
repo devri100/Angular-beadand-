9e28a608020df5e493cf620dd197bcf03932b651
devri100 <k.ricsi28@gmail.com>
2020-04-25 15:14:22 +0200

    Semmi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ListComponent} from "./components/list/list.component";
+import {LoginComponent} from "./components/login/login.component";
+import {AuthGuard} from "./guards/auth.guard";
+import {DetailsComponent} from "./components/details/details.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: "", redirectTo: "list", pathMatch: "full"},
+    {path: "list", component: ListComponent, canActivate: [AuthGuard]},
+    {path: "login", component: LoginComponent},
+    {path: "details/:id", component: DetailsComponent},
+    {path: "create", component: DetailsComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ListComponent} from './components/list/list.component';
+import {DetailsComponent} from './components/details/details.component';
+import {AuthService} from "./services/auth.service";
+import {EmployeeService} from "./services/employee.service";
+import {AuthGuard} from "./guards/auth.guard";
+import { LoginComponent } from './components/login/login.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ListComponent,
+        DetailsComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ReactiveFormsModule
+    ],
+    providers: [AuthService, EmployeeService, AuthGuard],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
